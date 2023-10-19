d6106a0593723148c59c28c5eebd83ba87ba79a7
Abhishek Patel <abhipatel9607@gmail.com>
2023-10-19 16:26:33 +0530

    Update async function chaining in Problem2

--- problem/problem2.js
+++ problem/problem2.js
@@ -1,43 +1,67 @@
-const fs = require("fs").promises
+const fs = require('fs').promises;
 
-
-async function main() {
-
-    const data = await fs.readFile("./lipsum.txt", "utf8");
-    await fs.writeFile("./uppercase.txt", data.toUpperCase())
-    await fs.writeFile("filenames.txt", "uppercase.txt\n")
-
-    const sentences = data.toLowerCase().split(".")
-    const allFileNames = []
-
-    sentences.forEach((sentence, i) => {
-        fs.writeFile(`./sentence${i}.txt`, sentence)
-        allFileNames.push(`sentence${i}.txt`)
-    })
-
-    await fs.writeFile("filenames.txt", allFileNames.join("\n"), { flag: "a" })
-
-
-    const allSortedContent = []
-
-    for (let i = 0; i < allFileNames.length; i++) {
-        let curFileName = allFileNames[i];
-        let curData = await fs.readFile(curFileName, "utf8");
-        let curSortedData = curData.split(" ").sort().join(" ")
-        allSortedContent.push(curSortedData)
-        await fs.writeFile(curFileName, curSortedData)
-    }
-
-    await fs.writeFile("./sorted.txt", allSortedContent.join("\n"), { flag: "a" })
-    await fs.writeFile("filenames.txt", "\n" + "sorted.txt", { flag: "a" })
-
-    const allDirectories = await fs.readFile("filenames.txt", "utf-8")
-    const allDirectoriesToDelete = allDirectories.split("\n")
-
-    for (let i = 0; i < allDirectoriesToDelete.length; i++) {
-        await fs.unlink(allDirectoriesToDelete[i])
-    }
-
+// Function to read a file and convert its content to uppercase
+async function convertToUppercase() {
+    const data = await fs.readFile('./lipsum.txt', 'utf8');
+    await fs.writeFile('./uppercase.txt', data.toUpperCase());
+    await fs.writeFile("./filenames.txt", "uppercase.txt\n");
+    return 'uppercase.txt';
 }
 
+// Function to split content into sentences, create separate files, and store filenames
+async function createSentencesFiles(uppercaseFileName) {
+    const data = await fs.readFile(uppercaseFileName, 'utf8');
+    const sentences = data.toLowerCase().split('.');
+    const allFileNames = [];
+
+    for (let i = 0; i < sentences.length; i++) {
+        const sentenceFileName = `sentence${i}.txt`;
+        await fs.writeFile(`./${sentenceFileName}`, sentences[i]);
+        allFileNames.push(sentenceFileName);
+    }
+    await fs.writeFile("./filenames.txt", allFileNames.join("\n"), { flag: "a" });
+    return allFileNames;
+}
+
+// Function to sort and rewrite the content of separate files
+async function sortAndRewriteFiles(fileNames) {
+    const allSortedContent = [];
+
+    for (let i = 0; i < fileNames.length; i++) {
+        const curFileName = fileNames[i];
+        const curData = await fs.readFile(curFileName, 'utf8');
+        const curSortedData = curData.split(' ').sort().join(' ');
+        await fs.writeFile(curFileName, curSortedData);
+        allSortedContent.push(curSortedData);
+    }
+
+    return allSortedContent;
+}
+
+// Function to delete all files mentioned in filenames.txt
+async function deleteFilesFromList() {
+    const allDirectories = await fs.readFile('filenames.txt', 'utf8');
+    const allDirectoriesToDelete = allDirectories.split('\n');
+
+    for (let i = 0; i < allDirectoriesToDelete.length; i++) {
+        await fs.unlink(allDirectoriesToDelete[i]);
+    }
+}
+
+// Main function to execute all steps in order
+async function main() {
+    try {
+        const uppercaseFileName = await convertToUppercase();
+        const sentenceFileNames = await createSentencesFiles(uppercaseFileName);
+        const sortedContents = await sortAndRewriteFiles(sentenceFileNames);
+        await fs.writeFile('./sorted.txt', sortedContents.join('\n'), { flag: 'a' });
+        await fs.writeFile("./filenames.txt", "\n" + "sorted.txt", { flag: "a" });
+        await deleteFilesFromList();
+        console.log('All tasks completed successfully.');
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// main();
 module.exports = main
